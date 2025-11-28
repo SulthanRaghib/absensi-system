d53d9ae17a417d2b59034f4fc47424c030aad33b
SulthanRaghib <raghib.smp@gmail.com>
2025-11-28 10:50:06 +0700

    feat: implement Jabatan resource with create, edit, and list pages

---
+++ app/Filament/Resources/Jabatans/Schemas/JabatanForm.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Filament\Resources\Jabatans\Schemas;
+
+use Filament\Schemas\Schema;
+use Filament\Forms\Components\TextInput;
+
+class JabatanForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->label('Nama Jabatan')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+}
