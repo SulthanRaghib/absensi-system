6672f760c2da2e6aac965e9e26c891990fd3d38e
SulthanRaghib <raghib.smp@gmail.com>
2025-11-28 13:30:16 +0700

    feat: add getRedirectUrl method to CreateRecord subclasses for consistent redirection

--- app/Filament/Resources/Absences/Pages/CreateAbsence.php
+++ app/Filament/Resources/Absences/Pages/CreateAbsence.php
@@ -8,4 +8,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateAbsence extends CreateRecord
 {
     protected static string $resource = AbsenceResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
--- app/Filament/Resources/Jabatans/Pages/CreateJabatan.php
+++ app/Filament/Resources/Jabatans/Pages/CreateJabatan.php
@@ -8,4 +8,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateJabatan extends CreateRecord
 {
     protected static string $resource = JabatanResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
