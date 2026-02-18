e2742d36f5534781d1f04de823efa301f0ef65b7
SulthanRaghib <raghib.smp@gmail.com>
2026-02-18 09:32:34 +0700

    feat: Schedule monthly holiday sync command to ensure timely data availability

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,12 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Schedule the holiday sync to run monthly
+use Illuminate\Support\Facades\Schedule;
+
+// Run on the last day of the month at 23:00 (11 PM)
+// Ensuring data is ready for the 1st of the next month.
+Schedule::command('app:sync-holidays')
+    ->lastDayOfMonth('23:00')
+    ->description('Sync national holidays from API for upcoming month');
